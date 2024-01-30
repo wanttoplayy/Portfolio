7d41c4e64217c25a1be4be00f671eb3f975d33a6
Vasin Suksuchit <bestwasin1617@gmail.com>
2024-01-30 23:59:02 +0700

    feat: about v2

--- components/About.tsx
+++ components/About.tsx
@@ -55,7 +55,7 @@ const qualificationData = [
     data: [
       {
         company: "Gosoft (CPALL)",
-        qualification: "Software Engineer",
+        role: "Software Engineer",
         years: "2023-Present",
       },
     ],
@@ -161,20 +161,141 @@ const About = () => {
                     <h3 className="h3 mb-8 text-center xl:text-left">
                       My Awesome Journey
                     </h3>
-                    <div>
+                    <div className="grid md:grid-cols-2 gap-y-8">
                       {/* experience */}
-                      <div className="flex gap-x-4 items-center text-[22px] text-primary">
-                        <Briefcase />
-                        <h4 className="capitalized font-medium">
-                          {getData(qualificationData, "experience").title}
-                        </h4>
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <Briefcase />
+                          <h4 className="capitalized font-medium">
+                            {getData(qualificationData, "experience").title}
+                          </h4>
+                        </div>
+                        {/* list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "experience").data.map(
+                            (item: any, index: any) => {
+                              const { company, role, years } = item;
+                              return (
+                                <div
+                                  className="flex gap-x-8 group bg-yellow-100"
+                                  key={index}
+                                >
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {company}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {role}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
                       </div>
+
                       {/* education */}
-                      <div>education</div>
+
+                      {/* experience */}
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <GraduationCap size={28} />
+                          <h4 className="capitalized font-medium">
+                            {getData(qualificationData, "education").title}
+                          </h4>
+                        </div>
+                        {/* list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "education").data.map(
+                            (item: any, index: any) => {
+                              const { university, qualification, years } = item;
+                              return (
+                                <div
+                                  className="flex gap-x-8 group bg-yellow-100"
+                                  key={index}
+                                >
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {university}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </TabsContent>
-                <TabsContent value="skills">skills info</TabsContent>
+                <TabsContent value="skills">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">What I use Everyday</h3>
+                    {/* skills */}
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2 ">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* skill list */}
+                      <div>
+                        {getData(skillData, "skills").data.map(
+                          (item: any, index: any) => {
+                            const { name } = item;
+                            return (
+                              <div
+                                className="w-2/4 text-center xl:text-left mx-auto xl:mx-0"
+                                key={index}
+                              >
+                                <div className="font-medium">{name}</div>
+                              </div>
+                            );
+                          }
+                        )}
+                      </div>
+                    </div>
+                    {/* tools */}
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                        Tools
+                      </h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* tools list */}
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, "tools").data.map(
+                          (item: any, index: any) => {
+                            const { imgPath } = item;
+                            return (
+                              <div key={index}>
+                                <Image
+                                  src={imgPath}
+                                  width={48}
+                                  height={48}
+                                  alt=""
+                                  priority
+                                />
+                              </div>
+                            );
+                          }
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </TabsContent>
               </div>
             </Tabs>
           </div>
